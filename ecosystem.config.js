95a1c1aa9daa971e9fc2b9e1b9c43a5a6bb21944
morgan.doria <morgan.doria@akeneo.com>
2021-05-14 17:41:19 +0200

    Add https

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
 apps: [
   {
     name: 'hello',
-    script: '/home/akeneo/partners-directory/releases/20210512162445/hello.js',
+    script: '/home/akeneo/partners-directory/releases/20210514135846/hello.js',
     watch: true,
     autorestart: true,
     restart_delay: 1000,
--- hello.js
+++ hello.js
@@ -1,15 +1,21 @@
-const http = require('http');
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/partners-directory-staging.ip.akeneo.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/partners-directory-staging.ip.akeneo.com/fullchain.pem')
+};
 
 const hostname = 'partners-directory-staging.ip.akeneo.com';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const server = https.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at https://${hostname}:${port}/`);
 });
 
