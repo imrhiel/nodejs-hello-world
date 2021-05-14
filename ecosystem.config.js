4fa4b59f73a97ab9a441e682e04d0d4168f1545d
morgan.doria <morgan.doria@akeneo.com>
2021-05-14 17:56:16 +0200

    Correct hello.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
 apps: [
   {
     name: 'hello',
-    script: '/home/akeneo/partners-directory/releases/20210514135846/hello.js',
+    script: '/home/akeneo/partners-directory/releases/20210514154157/hello.js',
     watch: true,
     autorestart: true,
     restart_delay: 1000,
--- hello.js
+++ hello.js
@@ -1,21 +1,22 @@
-const https = require('https');
-const fs = require('fs');
+var https = require('https');
+var fs = require('fs');
 
-const options = {
+var options = {
   key: fs.readFileSync('/etc/letsencrypt/partners-directory-staging.ip.akeneo.com/privkey.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/partners-directory-staging.ip.akeneo.com/fullchain.pem')
 };
 
-const hostname = 'partners-directory-staging.ip.akeneo.com';
-const port = 3000;
+var hostname = 'partners-directory-staging.ip.akeneo.com';
+var port = 3000;
 
-const server = https.createServer((req, res) => {
+var server = function (req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
-});
+};
 
-server.listen(port, hostname, () => {
+https.createServer(options, server).listen(port, hostname, () => {
   console.log(`Server running at https://${hostname}:${port}/`);
 });
+
 
